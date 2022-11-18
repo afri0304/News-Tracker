89ef89e9399dfdcc201d1b2dc0db8502826160aa
afri0304 <72117663+afri0304@users.noreply.github.com>
2022-11-19 00:27:17 +0530

    Delete template directory

--- template/server.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const app=express();
-
-app.use(express.static('/'));
-
-const server=app.listen(5000,()=>{
-	const port=server.address().port;
-	console.log("Server started at http://localhost:%s",port);
-});
